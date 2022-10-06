00f7da9e1222414747c96c37b118723f895a7cb8
Steve Krenn <stevekrenn@gmail.com>
2022-10-06 18:00:24 -0500

    JS citySearch operating, JS todo's commented

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 const APIkey = "f792ea86435d6d03093ac74cf0bb3026"
-var searchBtn = $('#search-btn')
-var city = $('#input')
-var searchHistory = $('#search-history')
+// var searchBtn = $('#search-btn')
+// var city = $('#input')
+// var searchHistory = $('#search-history')
 
 //Click Listener to Search Button
 // function citySearch(event) {
@@ -24,16 +24,24 @@ var searchHistory = $('#search-history')
 //Show 5 day forecast
 
 //Past Searches
-
-
+/////////////////////////
+// renderCurrentWeather = pass data from City Search
 
 //TUTOR INFO
 let searchInputEl = document.getElementById("input");
 let formTag = document.getElementById('form-test');
 
-function citySearch (event){
+function citySearch(event) {
     event.preventDefault();
-    console.log(searchInputEl.value);
+    // console.log(searchInputEl.value);
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInputEl.value + "&appid=" + APIkey;
+    console.log(queryURL)
+    fetch(queryURL)
+        .then((response) => response.json())
+        .then((data) => {
+
+            console.log(data)
+        });
 }
 
 formTag.addEventListener('submit', citySearch);
