8049f8a16c3325c73de4143309c3f36a93ab0e12
Steven Krenn <stevekrenn@gmail.com>
2022-10-09 20:27:24 -0500

    working variables for city conditions

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ const APIkey = "f792ea86435d6d03093ac74cf0bb3026"
 let searchInputEl = document.getElementById("input");
 let formTag = document.getElementById('form-test');
 let searchHistory = document.getElementById('search-history')
+let cityName = document.getElementById('city')
 
 //City Display Elements
 let currentTempEl = document.getElementById('temperature')
@@ -9,8 +10,8 @@ let currentCityEl = document.getElementById('city')
 let currentWindSpeedEl = document.getElementById('wind-speed')
 let currentHumidityEl = document.getElementById('humid')
 
-// Finding & Displaying weather for search city
-function citySearch(event) {
+// Current Conditions of City
+function currentWeather(event) {
     event.preventDefault();
     // console.log(searchInputEl.value);
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInputEl.value + "&appid=" + APIkey + "&units=imperial"
@@ -20,30 +21,23 @@ function citySearch(event) {
         .then((response) => response.json())
         .then((data) => {
             console.log(data)
-            //Get lat/longitude data
-            var lat = data.coord.lat;
-            var lon = data.coord.lon;
-            console.log(lat)
-            console.log(lon)
-
-            const {temp} = data.main
+            //Get Informational Data
+            const lat = data.coord.lat;
+            const lon = data.coord.lon;
             const place = data.name
-            const {description, icon} = data.weather[0]
-            console.log(temp)
-            console.log(place)
-            console.log(description)
-            console.log(icon)
+            // const { description, icon } = data.weather[0]
+            const temp = data.main.temp
+            const wind = data.wind.speed
+            const humidity = data.main.humidity
+            currentCityEl.textContent = place
+            currentTempEl.textContent = 'Current Temperature: ' + temp + ' °F'
+            currentWindSpeedEl.textContent = 'Current Windspeed: ' + wind + 'MPH'
+            currentHumidityEl.textContent = 'Current Humidity: ' + humidity + "%"
         });
 }
-
-function currentWeather() {
-
-}
-
-
 //Displaying 5 Days of Projected Weather Data
 function fiveDay() {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + lat + "&lon=" + "&units=imperial&cnt=6&appid=" + APIkey)
 }
 
-formTag.addEventListener('submit', citySearch);
+formTag.addEventListener('submit', currentWeather);
